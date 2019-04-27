eff019c9d5514166f320d2498ba1dd2e74eacd97
JerrySunny <ks.jerrysunny@gmail.com>
2019-04-27 14:14:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import MapView from './components/MapView';
 function App() {
   return (
     <div className="App">
@@ -17,7 +17,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
         </a>
+        <MapView></MapView>
       </header>
     </div>
   );
